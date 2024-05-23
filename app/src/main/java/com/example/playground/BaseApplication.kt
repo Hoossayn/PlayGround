eef613f22b338f17b6ea6274e4cd2c89b4d9cd59
hoossayn <abdulhakeemhusseni@gmail.com>
2024-05-23 22:53:35 +0100

    chore: (test) add navigation test for tv shows screen

--- app/src/main/java/com/example/playground/BaseApplication.kt
+++ app/src/main/java/com/example/playground/BaseApplication.kt
@@ -3,6 +3,5 @@ package com.example.playground
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
-
 @HiltAndroidApp
 class BaseApplication: Application()
--- screens/tvShows/build.gradle.kts
+++ screens/tvShows/build.gradle.kts
@@ -49,13 +49,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.activity.compose)
-    //testImplementation(project(":ui-test-hilt-manifest"))
+    testImplementation(project(":ui-test-hilt-manifest"))
 
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 
-    //testImplementation(projects.core.test)
-    //androidTestImplementation(projects.core.test)
+    testImplementation(projects.core.test)
+    androidTestImplementation(projects.core.test)
 }
