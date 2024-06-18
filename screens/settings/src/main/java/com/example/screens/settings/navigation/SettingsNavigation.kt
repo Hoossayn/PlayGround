c63a57c42f8f2262e9c6c58f3c630963959ea105
hoossayn <abdulhakeemhusseni@gmail.com>
2024-06-18 08:35:57 +0100

    chore: add settings screen navigation

---
+++ screens/settings/src/main/java/com/example/screens/settings/navigation/SettingsNavigation.kt
@@ -0,0 +1,22 @@
+package com.example.screens.settings.navigation
+
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
+import com.example.core.ui.navigation.enterTransition
+import com.example.core.ui.navigation.exitTransition
+import com.example.core.ui.navigation.popEnterTransition
+import com.example.core.ui.navigation.popExitTransition
+
+const val settingsNavPattern = "/settings"
+
+fun NavGraphBuilder.settingsScreen(onLoginClick: () -> Unit) {
+    composable(
+        route = settingsNavPattern,
+        enterTransition = enterTransition,
+        exitTransition = exitTransition,
+        popEnterTransition = popEnterTransition,
+        popExitTransition = popExitTransition,
+    ) {
+        SettingsScreen(onLoginClick = onLoginClick)
+    }
+}
