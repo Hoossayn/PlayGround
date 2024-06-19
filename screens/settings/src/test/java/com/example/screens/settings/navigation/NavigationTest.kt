890968af8950671254deb427aae56095f127202d
hoossayn <abdulhakeemhusseni@gmail.com>
2024-06-19 13:19:10 +0100

    chore: (test) add navigation test for settings screen

---
+++ screens/settings/src/test/java/com/example/screens/settings/navigation/NavigationTest.kt
@@ -0,0 +1,107 @@
+package com.example.screens.settings.navigation
+
+import android.os.Build
+import androidx.annotation.StringRes
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
+import androidx.navigation.compose.ComposeNavigator
+import androidx.navigation.compose.NavHost
+import androidx.navigation.testing.TestNavHostController
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.uitesthiltmanifest.HiltComponentActivity
+import com.google.common.truth.Truth
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import com.example.screens.settings.R
+import dagger.hilt.android.testing.HiltTestApplication
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.robolectric.annotation.Config
+import org.robolectric.shadows.ShadowLog
+import kotlin.properties.ReadOnlyProperty
+
+@RunWith(AndroidJUnit4::class)
+@Config(
+    application = HiltTestApplication::class,
+    sdk = [Build.VERSION_CODES.O],
+    instrumentedPackages = ["androidx.loader.content"],
+    qualifiers = "xlarge",
+)
+@HiltAndroidTest
+class NavigationTest {
+    /**
+     * Manages the components' state and is used to perform injection on your test
+     */
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    /**
+     * Create a temporary folder used to create a Data Store file. This guarantees that
+     * the file is removed in between each test, preventing a crash.
+     */
+    @BindValue
+    @get:Rule(order = 1)
+    val tmpFolder: TemporaryFolder = TemporaryFolder.builder().assureDeletion().build()
+
+    /**
+     * Use the primary activity to initialize the app normally.
+     */
+    @get:Rule(order = 2)
+    val composeTestRule = createAndroidComposeRule<HiltComponentActivity>()
+
+    private var onLoginClick = 0
+
+    private lateinit var navController: TestNavHostController
+
+    private fun AndroidComposeTestRule<*,*>.stringResource(
+        @StringRes resId: Int
+    ) = ReadOnlyProperty<Any?, String> {_,_ -> activity.getString(resId)}
+
+    private val login by composeTestRule.stringResource(R.string.login)
+
+    @Before
+    fun setUp() {
+        ShadowLog.stream = System.out
+
+        hiltRule.inject()
+    }
+
+    @Test
+    fun settingsScreen_onLoad_addSettingsScreenToNavHost() {
+        composeTestRule.apply {
+            setContent {
+                navController = TestNavHostController(LocalContext.current)
+                navController.navigatorProvider.addNavigator(ComposeNavigator())
+
+                NavHost(
+                    navController = navController,
+                    startDestination = settingsNavPattern
+                ) {
+                    settingsScreen(onLoginClick = { onLoginClick++ })
+                }
+            }
+
+            onNodeWithText(login)
+                .performClick()
+
+            Truth.assertThat(navController.currentDestination?.route).isEqualTo(settingsNavPattern)
+            Truth.assertThat(onLoginClick).isEqualTo(1)
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
